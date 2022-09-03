053419af9a2072fe9d60e7c9035e8166bcbd7458
Hernan Gonzalez <hernancho9207@gmail.com>
2022-09-03 17:31:21 -0500

    Estructura tags inicial

--- src/componentes/posthome/PostHome.js
+++ src/componentes/posthome/PostHome.js
@@ -7,16 +7,24 @@ const PostHome = props => {
                 <img src={props.data.image} className=" rounded card-img-top ImgSize" alt={props.data.publishDate} />
                 <div className="card-body ">
                     <h5 className="card-title">{props.data.owner.firstName + ' ' + props.data.owner.lastName}</h5>
+                    <p className='small'>{FormatearFecha(new Date(props.data.publishDate))}</p>
                     <p className="card-text">
                         <span className="small font-weight-light">{props.data.text}</span>
                     </p>
                 </div>
                 <div className='card-footer'>
-                  
+
                     <div className='text-left'>
-                    <i className="fa-regular fa-thumbs-up"></i> {props.data.likes} <span className='font-italic small'>{props.data.tags + ' '} </span>
+                        <i className="fa-regular fa-thumbs-up"></i> {props.data.likes + '  '}
+                        {props.data.tags.length === 0 ? (
+                            <div>Loading...</div>
+                        ) : (
+                            props.data.tags.map((e, i) => {
+                                return <Tags tag={e} key={i} />
+                            })
+                        )}
                     </div>
-                
+
                 </div>
             </div>
         </div>
@@ -24,5 +32,34 @@ const PostHome = props => {
     );
 }
 
+const Tags = props => {
+    return (
+        <span className="badge bg-info ">{props.tag + ' '}</span>
+
+
+    )
+}
+
+
+
+function fecha(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function FormatearFecha(date) {
+    return (
+        [
+            fecha(date.getDate()),
+            fecha(date.getMonth() + 1),
+            date.getFullYear(),
+        ].join('-') +
+        ' ' +
+        [
+            fecha(date.getHours()),
+            fecha(date.getMinutes()),
+            fecha(date.getSeconds()),
+        ].join(':')
+    );
+}
 
 export default PostHome;
